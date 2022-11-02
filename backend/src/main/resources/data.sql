73eace23440fde915f150203c289302c99f41104
Andrea Bolanos Mendez <andrea.bolanos.mendez@mail.utoronto.ca>
2022-11-01 22:08:42 -0400

    added more data in data.sql

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -4,10 +4,22 @@
 
 INSERT INTO UserProfile_E (Client_ID, First_Name, Last_Name, Age, Height, Weight) VALUES (1111, 'Daniel', 'Sapienza', 20, 176.0, 135.0);
 INSERT INTO UserProfile_E (Client_ID, First_Name, Last_Name, Age, Height, Weight) VALUES (2222, 'Erick', 'Romo', 15, 160.0, 100.0);
+INSERT INTO UserProfile_E (Client_ID, First_Name, Last_Name, Age, Height, Weight) VALUES (3333, 'Andrea', 'Mendez', 20, 177.5, 143.0);
+INSERT INTO UserProfile_E (Client_ID, First_Name, Last_Name, Age, Height, Weight) VALUES (4444, 'Lisa', 'Graham', 33, 154.7, 121.0);
+INSERT INTO UserProfile_E (Client_ID, First_Name, Last_Name, Age, Height, Weight) VALUES (5555, 'Julian', 'Geller', 45, 187.5, 156.0);
 
 
 INSERT INTO Exercise_E (Exercise_Id, Name, Muscle_Group, Repetitions, Heart_Beats_Per_Min) VALUES (123, 'Bench-Press', 'Chest', 4, 120);
 INSERT INTO Exercise_E (Exercise_Id, Name, Muscle_Group, Repetitions, Heart_Beats_Per_Min) VALUES (456, 'Pull-Up', 'Back', 5, 115);
+INSERT INTO Exercise_E (Exercise_Id, Name, Muscle_Group, Repetitions, Heart_Beats_Per_Min) VALUES (789, 'Russian twist', 'Abs', 12, 98);
+INSERT INTO Exercise_E (Exercise_Id, Name, Muscle_Group, Repetitions, Heart_Beats_Per_Min) VALUES (321, 'Barbell Hip Thrust', 'Glutes', 8, 103);
+INSERT INTO Exercise_E (Exercise_Id, Name, Muscle_Group, Repetitions, Heart_Beats_Per_Min) VALUES (654, 'Leg curls', 'Hamstrings', 10, 95);
 
 
 INSERT INTO Diet_E (Diet_Id, Name, Food_Group, Calories_Per_Serving, Sugar_Per_Serving, Protein_Per_Serving, Carbohydrates_Per_Serving) VALUES (2020,'Keto Diet', 'vegetables', 153.0, 23.5, 55.7, 14.3);
+INSERT INTO Diet_E (Diet_Id, Name, Food_Group, Calories_Per_Serving, Sugar_Per_Serving, Protein_Per_Serving, Carbohydrates_Per_Serving) VALUES (2021,'Dukan Diet', 'protein', 258.0, 9.7, 68.4, 11.2);
+INSERT INTO Diet_E (Diet_Id, Name, Food_Group, Calories_Per_Serving, Sugar_Per_Serving, Protein_Per_Serving, Carbohydrates_Per_Serving) VALUES (2022,'Atkins Diet', 'carbs', 133.0, 3.6, 16.3, 25.6);
+INSERT INTO Diet_E (Diet_Id, Name, Food_Group, Calories_Per_Serving, Sugar_Per_Serving, Protein_Per_Serving, Carbohydrates_Per_Serving) VALUES (2023,'Zone Diet', 'carbs', 184.0, 6.7, 22.6, 25.6);
+INSERT INTO Diet_E (Diet_Id, Name, Food_Group, Calories_Per_Serving, Sugar_Per_Serving, Protein_Per_Serving, Carbohydrates_Per_Serving) VALUES (2024,'Ultra-Low-Fat', 'vegetables', 95.0, 3.2, 15.8, 19.1);
+
+
