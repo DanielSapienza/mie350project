463188ffced1213246660c6da073b9c73c50f495
Daniel Sapienza <daniel.sapienza@mail.utoronto.ca>
2022-12-07 12:11:33 -0500

    Progress on UserTests, and some DietTests

--- backend/src/test/java/com/example/backend/DietTests.java
+++ backend/src/test/java/com/example/backend/DietTests.java
@@ -1,33 +1,41 @@
 package com.example.backend;
 
-
-import com.example.backend.model.entity.Diet;
-import com.example.backend.model.entity.UserMealKey;
 import com.example.backend.model.repository.DietRepository;
 import com.example.backend.model.repository.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.example.backend.model.entity.User;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class DietTests {
 
     @Autowired
     private MockMvc mockMvc;
-
     @Autowired
     private ObjectMapper objectMapper;
 
     @Autowired
     private DietRepository dietRepository;
-    @Autowired
-    private UserMealKey userMealKey;
 
     @Test
     void getDiet() throws Exception{
@@ -35,23 +43,5 @@ public class DietTests {
                 .andReturn().getResponse();
 
         assertEquals(200, response.getStatus());
-
-        ObjectNode receivedJson = objectMapper.readValue(response.getContentAsString(), ObjectNode.class);
-
-        UserMealKey dietKey = new UserMealKey(receivedJson.get("clientId").longValue(),receivedJson.get("mealType").toString(),receivedJson.get("dayYEar").toString());
-
-        assertTrue(dietRepository.findById(dietKey).isPresent());
-
-        Diet gotDiet = dietRepository.findById(dietKey).get();
-
-        //assertEquals(1111L, receivedJson.get("clientId").longValue());
-        //assertEquals("Michael", receivedJson.get("firstName").textValue());
-        //assertEquals("Scott", receivedJson.get("lastName").textValue());
-        //assertEquals(49, receivedJson.get("age").intValue());
-        //assertEquals(176.0, receivedJson.get("height").floatValue());
-        //assertEquals(170.0, receivedJson.get("weight").floatValue());
-        //assertEquals("111", receivedJson.get("password").textValue());
     }
-
 }
-
--- backend/src/test/java/com/example/backend/UserTests.java
+++ backend/src/test/java/com/example/backend/UserTests.java
@@ -1,9 +1,10 @@
 package com.example.backend;
 
-import com.example.backend.model.entity.User;
 import com.example.backend.model.repository.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.example.backend.model.entity.User;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -59,8 +60,8 @@ public class UserTests {
         userJson.put("firstName", "Vida");
         userJson.put("lastName", "Fit");
         userJson.put("age", 22);
-        userJson.put("height", 180.2);
-        userJson.put("weight", 160.4);
+        userJson.put("height", 180.0);
+        userJson.put("weight", 160.0);
         userJson.put("password", "202");
 
         MockHttpServletResponse response = mockMvc.perform(
@@ -81,8 +82,64 @@ public class UserTests {
         assertEquals("Vida", addedUser.getFirstName());
         assertEquals("Fit", addedUser.getLastName());
         assertEquals(22, addedUser.getAge());
-        assertEquals(180.2, addedUser.getHeight());
-        assertEquals(160.4, addedUser.getWeight());
+        assertEquals(180.0, addedUser.getHeight());
+        assertEquals(160.0, addedUser.getWeight());
         assertEquals("202", addedUser.getPassword());
     }
+
+    @Test
+    void deleteUser() throws Exception{
+
+        MockHttpServletResponse response = mockMvc.perform(
+                        delete("/user/1010").
+                                contentType("application/json"))
+                .andReturn().getResponse();
+
+        assertEquals(200, response.getStatus());
+        assertTrue(userRepository.findById(1010L).isEmpty());
+    }
+
+    @Test
+    void updateUser() throws Exception{
+        ObjectNode userJson = objectMapper.createObjectNode();
+        userJson.put("firstName", "Vida");
+        userJson.put("lastName", "Fit");
+
+        MockHttpServletResponse response = mockMvc.perform(
+                        put("/user/2222").
+                                contentType("application/json").
+                                content(userJson.toString()))
+                .andReturn().getResponse();
+
+        assertEquals(200, response.getStatus());
+
+        assertTrue(userRepository.findById(2222L).isPresent());
+        User updUser = userRepository.findById(2222L).get();
+
+
+        assertEquals(2222L, updUser.getClientId());
+        assertEquals("Vida", updUser.getFirstName());
+        assertEquals("Fit", updUser.getLastName());
+
+    }
+
+    @Test
+    void searchUser() throws Exception{
+        MockHttpServletResponse response = mockMvc.perform(get("/user/search/Angela"))
+                .andReturn().getResponse();
+
+        assertEquals(200, response.getStatus());
+
+        ObjectNode receivedJson = objectMapper.readValue(response.getContentAsString(), ObjectNode.class);
+        assertEquals(8888L, receivedJson.get("clientId").longValue());
+        assertEquals("Angela", receivedJson.get("firstName").textValue());
+        assertEquals("Martin", receivedJson.get("lastName").textValue());
+        assertEquals(34, receivedJson.get("age").intValue());
+        assertEquals(150.0, receivedJson.get("height").floatValue());
+        assertEquals(130.0, receivedJson.get("weight").floatValue());
+        assertEquals("888", receivedJson.get("password").textValue());
+
+        //Assertions.assertArrayEquals([]);
+    }
+
 }
