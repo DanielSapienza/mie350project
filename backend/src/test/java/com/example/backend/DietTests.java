5f29cac42905bc9df8ca7ade869e620d56aedef7
piroianv <victoria.piroian@mail.utoronto.ca>
2022-12-07 12:24:57 -0500

    diet get test

--- backend/src/test/java/com/example/backend/DietTests.java
+++ backend/src/test/java/com/example/backend/DietTests.java
@@ -1,5 +1,7 @@
 package com.example.backend;
 
+import com.example.backend.model.entity.Diet;
+import com.example.backend.model.entity.UserMealKey;
 import com.example.backend.model.repository.DietRepository;
 import com.example.backend.model.repository.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -43,5 +45,21 @@ public class DietTests {
                 .andReturn().getResponse();
 
         assertEquals(200, response.getStatus());
+        ObjectNode receivedJson = objectMapper.readValue(response.getContentAsString(), ObjectNode.class);
+
+        //UserMealKey dietKey = new UserMealKey(receivedJson.get("clientId").longValue(),receivedJson.get("mealType").textValue(),receivedJson.get("dayYear").textValue());
+        //assertTrue(dietRepository.findById(dietKey).isPresent());
+
+        //Diet gotDiet = dietRepository.findById(dietKey).get();
+
+        assertEquals(1111L, receivedJson.get("clientId").longValue());
+        assertEquals("Breakfast", receivedJson.get("mealType").textValue());
+        assertEquals("December2,2022", receivedJson.get("dayYear").textValue());
+        assertEquals("Eggs and toast", receivedJson.get("mealName").textValue());
+        assertEquals(300, receivedJson.get("calories").floatValue());
+        assertEquals(20.6, receivedJson.get("sugar").floatValue());
+        assertEquals(34.5, receivedJson.get("carbs").floatValue());
+        assertEquals(19.5, receivedJson.get("protein").floatValue());
+        assertEquals(12.7, receivedJson.get("fat").floatValue());
     }
 }
